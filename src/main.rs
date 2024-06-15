2153000dc4a50f9e06943f183adc1e998bc634bc
Blessing Pariola <blessingpariola@zoho.com>
2024-06-16 00:02:33 +0200

    feat: repl

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,24 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
     let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        let tokens = Vec::from_iter(input.split_whitespace());
+
+        if tokens.len() < 1 {
+            continue;
+        }
+
+        match tokens[0] {
+            "exit" => return,
+            command => println!("{command}: command not found"),
+        }
+    }
 }
